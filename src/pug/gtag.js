aa6d8e8aa7ebd2eb8b6b0a177f6009eb9c2f02b1
louiscklaw <louiscklaw@gmail.com>
2019-09-25 18:50:02 +0800

    update,

--- src/pug/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-73146416-10"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-73146416-10');
-</script>
