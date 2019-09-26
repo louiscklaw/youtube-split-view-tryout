fd2228758b7526aaed2611ec1716915de664fa29
louiscklaw <louiscklaw@gmail.com>
2019-09-26 14:21:22 +0800

    udpate,

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,67 @@
-const { src, dest, parallel } = require('gulp');
-const pug = require('gulp-pug');
-const less = require('gulp-less');
-const minifyCSS = require('gulp-csso');
-const concat = require('gulp-concat');
+// npm install gulp gulp-pug gulp-less gulp-csso gulp-concat gulp-javascript-obfuscator gulp-rename --save -D
+const {
+  src,
+  dest,
+  parallel,
+  series
+} = require( 'gulp' );
+const pug = require( 'gulp-pug' );
+const less = require( 'gulp-less' );
+const minifyCSS = require( 'gulp-csso' );
+const concat = require( 'gulp-concat' );
+
+const rename = require( 'gulp-rename' );
+const javascriptObfuscator = require( 'gulp-javascript-obfuscator' );
+
+const exec = require( 'child_process' ).exec;
 
 function html() {
-  return src('src/pug/index.pug')
-    .pipe(pug())
-    .pipe(dest('./docs'))
+  return src( './src/client/templates/index.pug' )
+      .pipe( pug() )
+      .pipe( dest( 'build' ) )
 }
 
 function css() {
-  return src('src/pug/*.css')
-    .pipe(less())
-    .pipe(minifyCSS())
-    .pipe(dest('./docs/css'))
+  return src( './src/client/templates/*.less' )
+      .pipe( less() )
+      .pipe( concat( 'style.css' ) )
+      .pipe( minifyCSS() )
+      .pipe( dest( 'build/css' ) )
 }
 
 function js() {
-  return src('src/pug/app.js', { sourcemaps: true })
-    // .pipe(concat('app.min.js'))
-    .pipe(dest('./docs/js', { sourcemaps: true }))
+  return src( [
+      './src/client/templates/const.js',
+      './src/client/templates/settings.js',
+      './src/client/templates/common.js',
+      './src/client/templates/app.js',
+  ], {
+          // sourcemaps: true
+      } )
+      .pipe( concat( 'app.js' ) )
+      .pipe( dest( 'build/js', {
+          // sourcemaps: true
+      } ) )
+}
+
+function js_compress() {
+  return src( './build/js/app.js' )
+      .pipe( javascriptObfuscator( {
+          compact: true
+      } ) )
+      .pipe( rename( 'app.min.js' ) )
+      // .pipe( sourcemaps.write() )
+      .pipe( dest( 'build/js', ) )
+}
+
+function deploy () {
+  return exec( 'rm -rf docs && cp -r ./src/build docs', (err, stdout, stderr) => {
+      console.log(stdout);
+  })
 }
 
 exports.js = js;
 exports.css = css;
 exports.html = html;
-
-exports.default = parallel(html, css, js);
+exports.default = parallel( css, series(js, js_compress),html );
+exports.deploy = deploy;
