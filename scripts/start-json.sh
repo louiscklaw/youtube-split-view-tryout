e2d85f879ce94b8dd091f59c409f04444910cac8
louiscklaw <louiscklaw@gmail.com>
2020-06-15 22:52:20 +0800

    update start-json to kill before start,

--- scripts/start-json.sh
+++ scripts/start-json.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+ROOT_NODE_PID=`sudo netstat -tpl|grep -i 3000|awk '{print $7}'|awk -F/ '{print $1}'`
+kill $ROOT_NODE_PID
+
 set -ex
+
+cd gatsby
 
 nodemon -e 'json' --exec json-server ./channel_list.json
 
